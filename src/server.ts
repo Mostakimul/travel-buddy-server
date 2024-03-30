d8b0a38091f63e33a12160b70292defa3d40c3d8
Mostakimul Karim <mostakimulkarim@gmail.com>
2024-03-30 08:12:46 -0400

    [feat]: server changes

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,29 @@ import { Server } from 'http';
 import app from './app';
 import { config } from './app/config';
 
+let server: Server;
+
 async function main() {
-  const server: Server = app.listen(config.port, () => {
-    console.log(`App is listening at port ${config.port}`);
-  });
+  try {
+    server = app.listen(config.port, () => {
+      console.log(`App is listening at port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 main();
+
+process.on('unhandledRejection', () => {
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  process.exit(1);
+});
